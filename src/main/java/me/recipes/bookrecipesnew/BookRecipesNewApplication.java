e761747070280b631e96473171dd008d73d203a5
OlgaJav22 <olga.ko4erga@yandex.ru>
2023-01-16 11:26:26 +0300

    начало

---
+++ src/main/java/me/recipes/bookrecipesnew/BookRecipesNewApplication.java
@@ -0,0 +1,13 @@
+package me.recipes.bookrecipesnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecipesNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRecipesNewApplication.class, args);
+    }
+
+}
